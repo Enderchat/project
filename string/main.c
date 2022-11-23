5df86a88a57d32590d6476ebd990b9e14d78260a
Enderchat <enderchat2015yandex.ru>
2022-11-23 15:15:16 +0700

    change

--- string/main.c
+++ string/main.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 #define SIZE 10
 
+int* calloc_strok1(const int size);
 char* calloc_stroka(const int size);
 char set_stroka(char *stroka);
 void print_stroka(char *stroka);
 char* delete_space(char *stroka);
-int chifri(*stroka);
+int* chifri(char *stroka);
 
 int main()
 {
@@ -16,6 +17,7 @@ int main()
 
     char *strok2 = calloc_stroka(SIZE);
     char *strok  = calloc_stroka(SIZE);
+    int *strok3  = calloc_strok1(SIZE);
     set_stroka(strok);
     /* print_stroka(strok);
      memcpy(strok2, strok, size);
@@ -28,14 +30,21 @@ int main()
 
     // printf("\n");
 
-    strok = delete_space(strok);
+    //strok = delete_space(strok);
 
+    printf("\n");
 
-
-    // printf("\n");
-
+    strok3 = chifri(strok);
     
+}
 
+int* calloc_strok1(const int size)
+{
+    int *a;
+
+    a = (int*)calloc(size, sizeof(int));
+    
+    return a;
 }
 
 char* calloc_stroka(const int size){
@@ -86,20 +95,30 @@ char* delete_space(char *stroka)
     return resultat;
 }
 
-int chifri(*stroka)
+int* chifri(char *stroka)
 {
-    int resultat[];
-    int len = strlen(stroka);
+    int len = strlen(stroka)-1;
+    int *resultat = (int*)calloc(len, sizeof(int));
     int j = 0;
+    int a = 0;
+
     for(int i = 0; i < len; ++i)
     {
-        if(stroka[i] >= 0 || stroka[i] <= 9)
+        if(stroka[i] >= '0' && stroka[i] <= '9')
         {
-            resultat[j] = stroka[i];
-            printf("%d \n", resultat[i]);
+            resultat[j]=stroka[i];
+            ++j;
+        }else if( j > 0 && (stroka[i] == ' ' ||  stroka[i] == '\n'))
+        {
+            printf("%s \n", resultat);
+            memset(resultat, '\0', len);
             j = 0;
         }
     }
-    printf("%s \n", resultat);
+    
+    for(int i = 0; i < len; ++i)
+    {
+        printf("%d", resultat[i]);
+    }
     return resultat;
 }
